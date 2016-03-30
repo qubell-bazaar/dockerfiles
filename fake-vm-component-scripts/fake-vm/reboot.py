4c1ca1fd2889198243e71a208ca2ee2958abcf0d
Roman Timushev <rtimush@gmail.com>
2016-03-30 19:29:31 +0300

    Reboot command for fake VMs

---
+++ fake-vm-component-scripts/fake-vm/reboot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+import yaml
+import time
+
+arguments = yaml.safe_load(sys.stdin)
+instances = arguments.get('instances', {})
+ids = list(instances.keys())
+
+vm_infos = {}
+for vmid in ids:
+    commands = instances.get(vmid, {}).get('commands', {})
+    commandids = list(commands.keys())
+    updates = {}
+    for commandid in commandids:
+        command = commands.get(commandid, {})
+        delay = command.get('info', {}).get('reboot', {}).get('delay', 0)
+        time.sleep(delay)
+        updates['commands.' + commandid] = [
+                { '$intermediate': True, 'message': "Rebooting" },
+                { 'message': "Done" }
+        ]
+    vm_infos[vmid] = {
+        '$pushAll': updates
+    }
+
+result = {
+    'instances': vm_infos
+}
+yaml.safe_dump(result, sys.stdout)
